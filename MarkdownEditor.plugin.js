14811eda74d2374dbadae1b95777b0363122ddce
ERKCreeper <117591514+ERKCreeper@users.noreply.github.com>
2023-07-22 07:03:59 -0400

    Create MarkdownEditor.plugin.js

---
+++ MarkdownEditor.plugin.js
@@ -0,0 +1,16 @@
+/**
+ * @name MarkdownEditor
+ * @author ERKCreeper
+ * @description Describe the basic functions. Maybe a support server link.
+ * @version 0.0.1
+ */
+
+module.exports = class MyPlugin {
+  start() {
+    // Do stuff when enabled
+  }
+
+  stop() {
+    // Cleanup when disabled
+  }
+};
